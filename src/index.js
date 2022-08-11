cf6567b9bbf78c7e384faf479712901b0df4f161
denis <denisvictorjosphine1996@gmail.com>
2022-08-11 22:55:11 +0530

    create popUp, fix layout and add colours

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './style.css';
 
-console.log('sdkhfksdf');
+document.querySelector('.mode').addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+});
+
+document.querySelector('.cancel').addEventListener('click',()=>{
+    document.querySelector('.popUpContainer').style.display = 'none';
+});
+
+document.querySelector('.menu').addEventListener('click',()=>{
+    document.querySelector('.sidebar').classList.toggle('expand');
+});
