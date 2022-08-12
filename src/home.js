28e4c0ee90f25be9fbd5b7690e8d9bd41a2799cb
denis <denisvictorjosphine1996@gmail.com>
2022-08-12 22:07:14 +0530

    create home page dynamically

---
+++ src/home.js
@@ -0,0 +1,21 @@
+export function home(){
+const content = document.querySelector('.content');
+content.innerHTML = '';
+const home_page = document.createElement('div');
+home_page.classList.add('home_page');
+content.append(home_page);
+const home1 = document.createElement('div');
+const home2 = document.createElement('div');
+home_page.append(home1,home2);
+
+
+    const home_header = document.createElement('div');
+    home_header.classList.add('home_header');
+    const hh1 = document.createElement('div');
+    hh1.textContent = 'Home';
+    const hh2 = document.createElement('div');
+    hh2.textContent = 'Due Date';
+    home_header.append(hh1,hh2);
+    home1.append(home_header);
+}
+
