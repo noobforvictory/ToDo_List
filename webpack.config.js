ff1a4f00a20f8840c40313a48d3a69bf85778224
denis <denisvictorjosphine1996@gmail.com>
2022-08-14 17:19:00 +0530

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ module.exports = {
   output: {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
     clean: false,
   },
   optimization: {
